5e88bf5ac7a530b0c7a77f86e82343fa8498661b
Wilfred Osozi <osoziw@gmail.com>
2025-04-06 15:02:25 +0300

    added about page content

--- nutri-assistant-app/src/Pages/About.jsx
+++ nutri-assistant-app/src/Pages/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div>
+      <h1>About Us</h1>
+      <p>Welcome to <strong>Healthy Life</strong> – your trusted companion in personal nutrition and wellness.</p>
+      </div>
+    </div>
   )
 }
 
